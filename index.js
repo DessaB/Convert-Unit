5f669de2a1c063aad624e1089af1f5d3531f4007
Dessa Brewington <DessaB@Magenic.com>
2017-05-12 13:24:11 -0500

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var Qty = require('js-quantities');
 console.log("Application started");
 
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res){
     res.json('Unit-Converter server running');
