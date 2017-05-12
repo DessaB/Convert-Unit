a01a6ebba87084d31a9b8b0430ec355a1cb616ad
Dessa Brewington <DessaB@Magenic.com>
2017-05-12 13:21:38 -0500

    .

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ var Qty = require('js-quantities');
 
 console.log("Application started");
 
-var result = "Result GET!!"
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res){
     res.json('Unit-Converter server running');
 });
 
 app.post('/convert', function (req, res){
-    var input = req.body.text || req.body;
-    var output = Qty.parse("input");
+    var input = req.body.text.toLowerCase();
+    var words = input.split("to");
+    var output = Qty(words[0]).to(words[1]);
+
+    //var output = Qty.parse(input) || "Invalid Unit Provided";
     res.send('Input = ' + input + ". Output = " + output);
 });
 
